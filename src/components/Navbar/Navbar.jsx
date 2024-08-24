fbd88d7bd83a36fc8a8320b0f9f1759f69b24eef
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2024-08-24 23:03:42 +0530

    Project Complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,8 +6,8 @@ import AvatarImage from '../../assets/spo.png'
 const Navbar = () => {
   return (
     <div className={styles.navbarContainer}>
-      <div className={styles.navItem}> <img src={Logo} alt="" /></div>
-    <img src={AvatarImage} alt="" className={styles.navImg}/>
+      <div className={styles.navItem}> <img src={Logo} alt="Logo" /></div>
+    <img src={AvatarImage} alt="User_image_random" className={styles.navImg}/>
     </div>
   );
 };
